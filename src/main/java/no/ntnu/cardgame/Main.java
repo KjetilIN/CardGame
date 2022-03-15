b2e8393ac5710a0867dee56ed67747e0337079be
KjetilMainPc <kjetilindrehus@gmail.com>
2022-03-16 00:08:55 +0100

    fix: change gui min-height

--- src/main/java/no/ntnu/cardgame/Main.java
+++ src/main/java/no/ntnu/cardgame/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
     public static void main(String[] args) {
@@ -16,8 +17,9 @@ public class Main extends Application {
         Scene scene = new Scene(loader.load());
         stage.setTitle("Card Application");
         stage.setScene(scene);
+        stage.initStyle(StageStyle.UTILITY);
         stage.setMinWidth(1200);
-        stage.setMinHeight(500);
+        stage.setMinHeight(800);
         stage.show();
     }
 }
